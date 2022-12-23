9bca5874261e5acdcd1ed0bd6676bb1bbed2c8cb
Edson <vieira.edson@cartazfacil.com>
2022-12-23 07:53:53 -0300

    Customizing PriceBox 0.1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
 import './App.css';
+//import { createDemoApp } from 'polotno/polotno-app';
+import '@blueprintjs/icons/lib/css/blueprint-icons.css';
+import '@blueprintjs/core/lib/css/blueprint.css';
+import '@blueprintjs/popover2/lib/css/blueprint-popover2.css';
+import { createStore } from 'polotno/model/store';
+import { PolotnoContainer, SidePanelWrap, WorkspaceWrap } from 'polotno';
+import { Toolbar } from 'polotno/toolbar/toolbar';
+import { ZoomButtons } from 'polotno/toolbar/zoom-buttons';
+import { SidePanel } from 'polotno/side-panel';
+import { Workspace } from 'polotno/canvas/workspace';
+import { setTranslations } from 'polotno/config';
+import { getTranslations } from 'polotno/config';
 
-function App() {
+setTranslations({
+  sidePanel: {
+    templates: 'Formatos',
+    text: 'Texto',
+    photos: 'Fotos',
+    elements: 'Formas',
+    upload: 'Enviar',
+    background: 'Fundo',
+    layers: 'Camadas',
+    resize: 'Redimensionar',
+  },
+});
+
+const store = createStore();
+/* Exemplo de criação DEMO
+const { store } = createDemoApp({
+  container: document.getElementById('root'),
+  key: 'PzZO8dQDYqDLAtXmjS2j', // you can create it here: https://polotno.com/cabinet/
+  // you can hide back-link on a paid license
+  // but it will be good if you can keep it for Polotno project support
+  showCredit: true,
+});
+*/
+
+const page = store.addPage();
+
+page.set({
+  // background can be any CSS color string or a url to the image
+  background: '#dde3eb',
+  // you can use "custom" attribute to save your own custom data
+  custom: { myInternalId: 'some-id-here' },
+  bleed: 10, // in pixels
+  width: 1000, // in pixels. You can use 'auto' to inherit width from the store
+  height: 1000, // in pixels. You can use 'auto' to inherit height from the store
+});
+
+page.addElement({
+  x: 50,
+  y: 50,
+  type: 'text',
+  fill: 'black',
+  text: 'Apenas testando algumas palavras',
+});
+console.log(getTranslations());
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <PolotnoContainer style={{ width: '100vw', height: '100vh' }}>
+      <SidePanelWrap>
+        <SidePanel store={store} />
+      </SidePanelWrap>
+      <WorkspaceWrap>
+        <Toolbar store={store} downloadButtonEnabled />
+        <Workspace store={store} />
+        <ZoomButtons store={store} />
+      </WorkspaceWrap>
+    </PolotnoContainer>
   );
 }
 
-export default App;
+export default App
